1ace7c36bf14e460b39055ee849a9d24e022215d
Sari-Katz <Hand4d17@mbjcomp.org.il>
2025-06-17 12:32:15 +0300

    Home

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -4,7 +4,10 @@ import styles from "./Footer.module.css";
 function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>© 2025 סטודיו לכושר - כל הזכויות שמורות</p>
+            <p>טובה-מאמנת פילאטיס וכושר</p>
+            <p>0548493746</p>
+            <p>tova746@gmail.com</p>
+            <p>הקונגרס הציוני 11. 360 מעל הקניון</p>
     </footer>
   );
 }
