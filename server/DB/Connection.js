b740e31f82e268f57a13fca6a4cee60caee568b6
necha <nechami191@gmail.com>
2025-12-23 14:33:33 +0200

    Add .dockerignore files for client and server

--- server/DB/Connection.js
+++ server/DB/Connection.js
@@ -1,6 +1,6 @@
 // dbConnection.js
 const mysql = require('mysql2/promise');
-require('dotenv').config({ path: '../../.env' }); 
+require('dotenv').config({ path: '../.env' }); 
 const pool = mysql.createPool({
   host:process.env.DB_HOST,
   user: process.env.DB_USER,
